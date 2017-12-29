0dc2e82e1158968aa18ac9d4f8d83498f6a0aa0e
Ryan Bester <ryan.bester5@gmail.com>
2017-12-29 12:08:03 +0000

    Created a blank window Added icon

---
+++ RSSReaderWindows/Main.cpp
@@ -0,0 +1,62 @@
+#include <Windows.h>
+#include "Resource.h"
+
+static TCHAR szClassName[] = TEXT("RSS Reader");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+	HWND hwnd;
+	MSG msg;
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = 0;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPICON));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szClassName;
+	wcex.hIconSm = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_APPICON), IMAGE_ICON, 16, 16, 0);
+
+	if (!RegisterClassEx(&wcex)) {
+		MessageBox(NULL, TEXT("Failed to register the window class"), szClassName, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	hwnd = CreateWindowEx(0, szClassName, TEXT("RSS Reader"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) {
+		MessageBox(NULL, TEXT("Failed to create the window"), szClassName, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_CREATE:
+	{
+		break;
+	}
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		break;
+	}
+	case WM_QUIT:
+	{
+		DestroyWindow(hwnd);
+		break;
+	}
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
